2ed4e43e34cb4cbd7f4de8d364fc7656bf893dda
Никита Ярыгин <nik.yarygin.2002@mail.ru>
2021-06-03 20:53:40 +0300

    Enter

--- RedBlackTree.cpp
+++ RedBlackTree.cpp
@@ -3,20 +3,22 @@
 
 #include "RedBlackTree.h"
 
+int main() {
 
+  tree t;
+  t.insert(7);
+  t.insert(5);
+  t.insert(3);
+  t.insert(6);
+  t.insert(4);
+  t.left_rotation(t.root->left.get());
 
+  std::cout << std::endl;
+  std::cout << t.root->left->left->left->right->value << std::endl;
+  std::cout << t.root->left->left->left->value << std::endl;
+  std::cout << t.root->left->left->value << std::endl;
+  std::cout << t.root->left->value << std::endl;
+  std::cout << t.root->value << std::endl;
 
-int main(){
-
-    tree t;
-    t.insert(5);
-    
-    std::cout<<t.root->value<<std::endl;
-
-
-
-    return 0;
-
-
-
+  return 0;
 }
