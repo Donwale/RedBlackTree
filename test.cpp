c3711d0927a1ecd53e0f92f567c599a3146c28d2
Никита Ярыгин <nik.yarygin.2002@mail.ru>
2021-06-03 21:02:45 +0300

    e

---
+++ test.cpp
@@ -0,0 +1,24 @@
+﻿// RedBlackTree.cpp: определяет точку входа для приложения.
+//
+
+#include "RedBlackTree.h"
+
+int main() {
+
+  tree t;
+  t.insert(7);
+  t.insert(5);
+  t.insert(3);
+  t.insert(6);
+  t.insert(4);
+  t.left_rotation(t.root->left.get());
+
+  std::cout << std::endl;
+  std::cout << t.root->left->left->left->right->value << std::endl;
+  std::cout << t.root->left->left->left->value << std::endl;
+  std::cout << t.root->left->left->value << std::endl;
+  std::cout << t.root->left->value << std::endl;
+  std::cout << t.root->value << std::endl;
+
+  return 0;
+}
